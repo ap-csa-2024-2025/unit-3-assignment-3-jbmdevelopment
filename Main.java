b63d76b707f12c1184d824dd600167c151907e65
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-10-29 11:03:00 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,57 @@ public class Main
   public static void main(String[] args)
   {
     // TODO: Problem 1
-
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter an integer");
+    int int1 = input.nextInt();
+    System.out.println("Enter another integer");
+    int int2 = input.nextInt();
+    if(int2 == 0) {
+        int num = (int)(Math.random() * 10) + 1;
+        System.out.println("Cannot divide by zero auto chosen number to " + num);
+    } else {
+        final double ratio = int1 / int2;
+        if(ratio > 1 && ratio <= 8) {
+            System.out.println("Ratio OK");
+        } else {
+          System.out.println("This ratio is terrible");
+        }
+    }
 
     // TODO: Problem 2
-
-
+    System.out.println("Give an integer");
+    int num1 = input.nextInt();
+    System.out.println("Give another integer");
+    int num2 = input.nextInt();
+    if(num2 == 0) {
+        int num = (int)(Math.random() * 1000) + 1;
+        System.out.println("Cannot divide by zero auto chosen number to " + num);
+    } else {
+      if(num1 % num2 == 0) {
+          System.out.println("Is a factor");
+      } else {
+        System.out.println("Is not a factor");
+      }
+    }
     // TODO: Problem 3
+    System.out.println("Enter an integer in the fifties");
+    int another = input.nextInt();
+    if(another < 50 || another > 59) {
+        System.out.println("That's not in the fifties!");
+    }
+    System.out.println("Your number is " + another);
+    System.out.println("Enter an int");
+    int x = input.nextInt();
+    System.out.println("Enter another integer");
+    int y = input.nextInt();
+    if(y <= 9 || !(x <= 2 && x * y < 10)) {
+        System.out.println("pass");
+    }
+    
+    // warm up
+    Rectangle r1 = new Rectangle(5, 10);
+    Rectangle r2 = new Rectangle(5, 10);
+    boolean isEqual = rectEqual(r1, r2);
   }
 
   // TODO: In-class assignment
@@ -21,6 +66,6 @@ public class Main
   public static boolean rectEqual(Rectangle r1, Rectangle r2)
   {
     // implement solution here
-    return false;
+    return r1.getWidth() == r2.getWidth() && r1.getLength() == r2.getLength();
   }
 }
